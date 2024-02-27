d8dea23d68f5c7f90c71c62efb4fdf39aaf65abc
Francisco Cano <volublecoffe@gmail.com>
2024-02-26 21:54:36 -0600

    Commi

---
+++ MathOperation.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_U3_4_metodos
+{
+    class MathOperation
+    {
+        // Attributes
+        private double number1;
+        private double number2;
+        private double result;
+
+        // Constructor
+        public MathOperation(double num1, double num2)
+        {
+            number1 = num1;
+            number2 = num2;
+        }
+
+        // Method to perform the mathematical operation
+        public void PerformOperation()
+        {
+            result = number1 + number2;
+        }
+
+        // Read and write methods
+        public void SetNumbers(double num1, double num2)
+        {
+            number1 = num1;
+            number2 = num2;
+        }
+
+        public double GetResult()
+        {
+            return result;
+        }
+
+        // Methods without reading and with writing
+        public void SetResult(double res)
+        {
+            result = res;
+        }
+
+        // Methods with reading and without writing
+        public double GetNumber1()
+        {
+            return number1;
+        }
+
+        public double GetNumber2()
+        {
+            return number2;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using POO_U3_4_metodos;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create an object of the MathOperation class
+        MathOperation operation = new MathOperation(0, 0);
+
+        // Use method without reading and with writing
+        operation.SetResult(0);
+
+        // Use method with reading and without writing
+        double number1 = operation.GetNumber1();
+        double number2 = operation.GetNumber2();
+
+        // Use method with reading and writing
+        operation.SetNumbers(8, 4);
+        operation.PerformOperation();
+        double result = operation.GetResult();
+
+        // Use method without reading and without writing
+        Console.WriteLine("The result of the operation is: " + result);
+    }
+}
